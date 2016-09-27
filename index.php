7caac9af430340776a28e252341ac490acd19665
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-27 20:44:38 +0100

    Added a basic index file.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// The file defines all index page behaviours.
+
+// Grab the header.
+get_header(); ?>
+
+<!-- site-content -->
+<div class="site-content clearfix">
+	
+</div><!-- /site-content -->
+
+<?php
+// Grab the footer.
+get_footer();
+?>
