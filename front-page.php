2033e46debc3cee6ce344b7ac64c718eec4a876f
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-27 20:49:20 +0100

    Added skeleton front page, and activated static homepage behaviour.

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+// The file defines the static home page behaviours.
+
+// Grab the header.
+get_header(); ?>
+
+<!-- site-content -->
+<div class="site-content clearfix">
+	
+</div><!-- /site-content -->
+
+<?php
+// Grab the footer.
+get_footer();
+?>
