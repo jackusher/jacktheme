e4d565d649eaaaf3081052b3d984ab3767d007b6
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-27 20:39:33 +0100

    First commit.

---
+++ footer.php
@@ -0,0 +1,20 @@
+	<!-- Defining the footer class and content in this file. -->
+	<footer class="site-footer">
+		
+		<div class="footer-meta">
+
+			<!-- Creating the 'copyright' div location for copyright info. Define content. -->
+			<section class="copyright">
+				<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
+			</section>
+			
+		</div>
+	
+	</footer>
+
+</div><!-- /container -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<!-- This file defines the content and behaviour of the theme header. -->
+	<!-- Putting header information in place, including fonts. -->
+	<head>
+		<link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width">
+		<title><?php bloginfo('name'); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	
+<body <?php body_class(); ?>>
+	
+	<!-- The div 'container' includes all visible site content inside the margins. -->
+	<div class="container">
+	
+		<!-- site-header begins. -->
+		<header class="site-header">
+			
+		</header><!-- /site-header -->
