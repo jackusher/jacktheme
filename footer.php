12e90abf7cb4a46abe519724196f3a08c0bbde54
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-29 01:06:58 +0100

    Completed style and layout of footer.

--- footer.php
+++ footer.php
@@ -1,14 +1,25 @@
 	<!-- Defining the footer class and content in this file. -->
 	<footer class="site-footer">
 		
-		<div class="footer-meta">
-
-			<!-- Creating the 'copyright' div location for copyright info. Define content. -->
-			<section class="copyright">
-				<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
-			</section>
-			
-		</div>
+		<section id="footer-contact" class="footer">
+			<p>email: <a href="mailto:<?php bloginfo('admin_email'); ?>">info@jackusher.co.uk</a></p>
+			<p>location: Durham and London, UK</p>
+		</section><!-- /footer-contact -->
+		
+		<section id="footer-sitemap" class="footer">
+			<nav class="site-nav">
+				<?php
+				$args = array(
+					'theme_location' => 'footer'
+				);
+				wp_nav_menu( $args ); ?>
+			</nav>
+		</section>
+		
+		<!-- Creating the 'copyright' location for copyright info. -->
+		<section id="footer-copyright" class="footer">
+			<p>&copy; <?php bloginfo('name'); ?>, <?php echo date('Y');?></p>
+		</section><!-- /footer-copyright -->
 	
 	</footer>
 
