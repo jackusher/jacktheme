e2725a65d9cb7097293ecf2499940b79e9dd2ffc
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-29 23:56:00 +0100

    Refined single page markup.

--- page.php
+++ page.php
@@ -10,17 +10,12 @@ get_header(); ?>
 	<?php
 	if (have_posts()) :
 		while (have_posts()) : the_post(); ?>
-	
-		<!-- Definte the article class for page layout and content. -->
-		<article class="page">
 		
 			<!-- Bringing in the basic content blocks (title and content). -->
 			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
-				
-		</article><!-- /post page -->
+			<?php the_content();
 	
-		<?php endwhile;
+		endwhile;
 	
 		// What to do if there's no content.
 	else :
